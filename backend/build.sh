4cab774421caf8978119edd6699d8e0bb59b5bf5
consolexyz <mosesobike@gmail.com>
2025-06-18 19:34:30 +0100

    fix requirements file

--- backend/build.sh
+++ backend/build.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
+source ~/.cargo/env
+pip install --upgrade pip
 pip install -r requirements.txt
 gunicorn --bind 0.0.0.0:$PORT server:app
